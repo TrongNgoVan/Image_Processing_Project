29ee3e1fa9d85b60e83bc7168fcdb164ffd1d2f1
Ngọ Văn Trọng <ngovantrong1308@gmail.com>
2024-11-08 20:37:02 +0700

    Hoàn thành cơ bản web demo Xử lý Ảnh y tế

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template, jsonify
 import numpy as np
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import img_to_array, load_img
+from tensorflow.keras.applications.vgg16 import preprocess_input 
 import io
 from PIL import Image
 
@@ -28,17 +29,21 @@ def predict():
     img_bytes = file.read()
     image = Image.open(io.BytesIO(img_bytes))
 
-    # Chỉnh lại kích thước và chuyển đổi ảnh thành mảng NumPy
-    image = image.resize((224, 224))  # Đảm bảo kích thước phù hợp với mô hình của bạn
-    image = img_to_array(image)
-    image = np.expand_dims(image, axis=0)  # Thêm chiều batch để mô hình có thể dự đoán
+    # Chỉnh lại kích thước ảnh (theo kích thước huấn luyện là 150x150)
+    image = image.resize((150, 150))  # Đảm bảo kích thước đúng với mô hình huấn luyện
 
+    # Chuyển đổi ảnh thành mảng NumPy
+    image = img_to_array(image)
+    
+    # Chuẩn hóa ảnh giống như trong quá trình huấn luyện (1./255)
+    image = image / 255.0  # Hoặc sử dụng preprocess_input nếu cần
+    image = np.expand_dims(image, axis=0)  # Thêm chiều batch
+    
     # Dự đoán
     prediction = model.predict(image)
     predicted_class = np.argmax(prediction, axis=1)[0]
 
     # Trả kết quả dưới dạng JSON
     return jsonify({'predicted_class': int(predicted_class)})
-
 if __name__ == '__main__':
     app.run(debug=True)
